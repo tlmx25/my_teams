799c80592753fc76bcc3a3dace88c831e415f97e
justine <justine.loizel@gmail.com>
2024-04-12 12:47:17 +0200

    fix: messages command cleaned

--- src/server/src/commands/messages_command.c
+++ src/server/src/commands/messages_command.c
@@ -11,11 +11,21 @@
 #include "time_utils.h"
 #include "my.h"
 
+static void send_request(client_t *client, message_t *msg, char *uuid_str)
+{
+    char *body = my_strcat(uuid_str, "\r");
+
+    body = my_strcat_free(body, timestamp_to_str(msg->timestamp), 1, 0);
+    body = my_strcat_free(body, "\r", 1, 0);
+    body = my_strcat_free(body, msg->message, 1, 0);
+    add_request_to_list(client->requests_sent,
+        create_request(PRINT_PRV_MSG, 200, body));
+}
+
 static void print_messages(client_t *current_client, client_t *client,
     server_t *server)
 {
     message_t *current_message = server->messages->head;
-    char *body;
     char uuid_str_sender[37] = {0};
     char uuid_str_receiver[37] = {0};
     char uuid_str_client[37] = {0};
@@ -27,23 +37,11 @@ static void print_messages(client_t *current_client, client_t *client,
         uuid_unparse(current_message->sender_uuid, uuid_str_sender);
         uuid_unparse(current_message->receiver_uuid, uuid_str_receiver);
         if (my_strcmp(uuid_str_receiver, uuid_str_current_client) == 0
-            && my_strcmp(uuid_str_sender, uuid_str_client) == 0) {
-            body = my_strcat(uuid_str_sender, "\r");
-            body = my_strcat_free(body, timestamp_to_str(current_message->timestamp), 1, 0);
-            body = my_strcat_free(body, "\r", 1, 0);
-            body = my_strcat_free(body, current_message->message, 1, 0);
-            add_request_to_list(client->requests_sent,
-                create_request(PRINT_PRV_MSG, 200, body));
-        }
+            && my_strcmp(uuid_str_sender, uuid_str_client) == 0)
+            send_request(client, current_message, uuid_str_sender);
         if (my_strcmp(uuid_str_receiver, uuid_str_client) == 0
-            && my_strcmp(uuid_str_sender, uuid_str_current_client) == 0) {
-            body = my_strcat(uuid_str_receiver, "\r");
-            body = my_strcat_free(body, timestamp_to_str(current_message->timestamp), 1, 0);
-            body = my_strcat_free(body, "\r", 1, 0);
-            body = my_strcat_free(body, current_message->message, 1, 0);
-            add_request_to_list(client->requests_sent,
-                create_request(PRINT_PRV_MSG, 200, body));
-        }
+            && my_strcmp(uuid_str_sender, uuid_str_current_client) == 0)
+            send_request(client, current_message, uuid_str_receiver);
         current_message = current_message->next;
     }
 }
