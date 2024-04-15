01b01a974bd22ec7b05fc8314a674f864463dc03
Sebabacou <sebastien.bertilsouchet@protonmail.com>
2024-04-15 14:13:50 +0200

    fix: bad answer

--- src/server/src/commands/info_command.c
+++ src/server/src/commands/info_command.c
@@ -6,12 +6,16 @@
 */
 
 #include "server.h"
+#include "command_server.h"
+#include "request.h"
+#include "my.h"
 
 void info_command(server_t *server, client_t *client, char **command)
 {
 	if (client->is_connected == false) {
 		add_request_to_list(client->requests_sent,create_request
-		(PRINT_ERROR, 200, "You are not logged in"));
+		(PRINT_ERROR, 401, "You are not logged in"));
 		return;
 	}
+
 }
