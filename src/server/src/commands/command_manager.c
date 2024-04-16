8bf80053abd65915bea08c7b1e9a3750f74ecadc
Tom <tom.lefoix@epitech.eu>
2024-04-16 10:34:08 +0200

    fix: command name

--- src/server/src/commands/command_manager.c
+++ src/server/src/commands/command_manager.c
@@ -14,6 +14,7 @@ static const command_t COMMANDS[] = {
     {"/login", login_command, false, 1},
     {"/li", login_command, false, 1},
     {"/lo", logout_command, true, 0},
+    {"/logout", logout_command, true, 0},
     {"/help", help_command, false, 0},
     {"/users", users_command, true, 0},
     {"/user", user_command, true, 1},
