44c5501e4dc7e4bd94a772634eebaa092e5579e9
Sebabacou <sebastien.bertilsouchet@protonmail.com>
2024-04-17 10:41:56 +0200

    fix: better code

--- src/server/src/commands/info_command.c
+++ src/server/src/commands/info_command.c
@@ -22,40 +22,41 @@ static bool_t error_non_existing(team_t *team, channel_t *channel,
     return true;
 }
 
-team_t find_team(server_t *server, char *uuid_str)
+void send_team_display_info(char *body, client_t *client, team_t *team)
 {
-    team_t *team = server->teams->head;
-
-    while (team) {
-        if (my_strcmp(uuid_str, team->uuid) == 0)
-            return *team;
-        team = team->next;
-    }
-    return (channel_t){0};
+    body = my_strcat_free(body, team->name, 1, 0);
+    body = my_strcat_free(body, "\r", 1, 0);
+    body = my_strcat_free(body, team->description, 1, 0);
+    my_strncpy(body, body, MAX_BODY_LENGTH);
+    create_add_request_to_list(client->requests_sent, NT_PRINT_TEAM, 200,
+        body);
+    free(body);
 }
 
-channel_t find_channel(server_t *server, char *uuid_str)
+void send_channel_display_info(char *body, client_t *client, channel_t
+    *channel)
 {
-    channel_t *channel = server->channels->head;
-
-    while (channel) {
-        if (my_strcmp(uuid_str, channel->uuid) == 0)
-            return *channel;
-        channel = channel->next;
-    }
-    return (channel_t){0};
+    body = my_strcat_free(body, channel->name, 1, 0);
+    body = my_strcat_free(body, "\r", 1, 0);
+    body = my_strcat_free(body, channel->description, 1, 0);
+    my_strncpy(body, body, MAX_BODY_LENGTH);
+    create_add_request_to_list(client->requests_sent, NT_PRINT_CHANNEL, 200,
+        body);
+    free(body);
 }
 
-thread_t find_thread(server_t *server, char *uuid_str)
+void send_thread_display_info(char *body, client_t *client, thread_t *thread)
 {
-    thread_t *thread = server->threads->head;
-
-    while (thread) {
-        if (my_strcmp(uuid_str, thread->uuid) == 0)
-            return *thread;
-        thread = thread->next;
-    }
-    return (thread_t){0};
+    body = my_strcat_free(body, "\r", 1, 0);
+    body = my_strcat_free(body, thread->created_at, 1, 0);
+    body = my_strcat_free(body, "\r", 1, 0);
+    body = my_strcat_free(body, thread->title, 1, 0);
+    body = my_strcat_free(body, "\r", 1, 0);
+    body = my_strcat_free(body, thread->message, 1, 0);
+    my_strncpy(body, body, MAX_BODY_LENGTH);
+    create_add_request_to_list(client->requests_sent, NT_PRINT_THREAD, 200,
+        body);
+    free(body);
 }
 
 static void user_display_info(client_t *client)
@@ -71,30 +72,30 @@ static void user_display_info(client_t *client)
     body = my_strcat_free(body, "\r", 1, 0);
     my_strncpy(body, body, MAX_BODY_LENGTH);
     create_add_request_to_list(client->requests_sent, NT_PRINT_USER, 200,
-                               body);
+        body);
     free(body);
 }
 
 static void team_display_info(server_t *server, client_t *client)
 {
     char uuid_str[37] = {0};
-    char uuid_str_tmp[37] = {0};
+    char uuid_str_client[37] = {0};
     char *body;
-    team_t *team = server->teams->head;
+    team_t *team;
 
-    uuid_unparse(client->context->uuid_team, uuid_str_tmp);
-    team = find_team(server, uuid_str_tmp);
-    if (error_non_existing(team, NULL, NULL, client) == false)
+    uuid_unparse(client->context->uuid_team, uuid_str_client);
+    if (error_non_existing(server->teams->head, NULL, NULL, client) == false)
         return;
-    body = my_strcat(uuid_str, "\r");
-    body = my_strcat_free(body, team->name, 1, 0);
-    body = my_strcat_free(body, "\r", 1, 0);
-    body = my_strcat_free(body, team->description, 1, 0);
-    body = my_strcat_free(body, "\r", 1, 0);
-    my_strncpy(body, body, MAX_BODY_LENGTH);
-    create_add_request_to_list(client->requests_sent, NT_PRINT_TEAM, 200,
-                               body);
-    free(body);
+    team = server->teams->head;
+    while (team) {
+        uuid_unparse(team->uuid, uuid_str);
+        if (my_strcmp(uuid_str, uuid_str_client) == 0) {
+            body = my_strcat(uuid_str, "\r");
+            send_team_display_info(body, client, team);
+            return;
+        }
+        team = team->next;
+    }
 }
 
 static void channel_display_info(server_t *server, client_t *client)
@@ -102,35 +103,22 @@ static void channel_display_info(server_t *server, client_t *client)
     char uuid_str[37] = {0};
     char uuid_str_tmp[37] = {0};
     char *body;
-    channel_t *channel = server->channels->head;
+    channel_t *channel;
 
     uuid_unparse(client->context->uuid_channel, uuid_str_tmp);
-    channel = find_channel(server, uuid_str_tmp);
-    if (error_non_existing(NULL, channel, NULL, client) == false)
+    if (error_non_existing(NULL, server->channels->head, NULL, client) ==
+    false)
         return;
-    uuid_unparse(channel->uuid, uuid_str);
-    body = my_strcat(uuid_str, "\r");
-    body = my_strcat_free(body, channel->name, 1, 0);
-    body = my_strcat_free(body, "\r", 1, 0);
-    body = my_strcat_free(body, channel->description, 1, 0);
-    body = my_strcat_free(body, "\r", 1, 0);
-    my_strncpy(body, body, MAX_BODY_LENGTH);
-    create_add_request_to_list(client->requests_sent, NT_PRINT_CHANNEL, 200,
-                               body);
-    free(body);
-}
-
-void send_thread_display_info(char *body, client_t *client, thread_t thread)
-{
-    body = my_strcat_free(body, thread->created_at, 1, 0);
-    body = my_strcat_free(body, "\r", 1, 0);
-    body = my_strcat_free(body, thread.title, 1, 0);
-    body = my_strcat_free(body, "\r", 1, 0);
-    body = my_strcat_free(body, thread.message, 1, 0);
-    my_strncpy(body, body, MAX_BODY_LENGTH);
-    create_add_request_to_list(client.requests_sent, NT_PRINT_THREAD, 200,
-                               body);
-    free(body);
+    channel = server->channels->head;
+    while (channel) {
+        uuid_unparse(channel->uuid, uuid_str);
+        if (my_strcmp(uuid_str, uuid_str_tmp) == 0) {
+            body = my_strcat(uuid_str, "\r");
+            send_channel_display_info(body, client, channel);
+            return;
+        }
+        channel = channel->next;
+    }
 }
 
 static void thread_display_info(server_t *server, client_t *client)
@@ -141,15 +129,19 @@ static void thread_display_info(server_t *server, client_t *client)
     thread_t *thread = server->threads->head;
 
     uuid_unparse(client->context->uuid_thread, uuid_str_client);
-    thread = find_thread(server, uuid_str_client);
     if (error_non_existing(NULL, NULL, thread, client) == false)
         return;
-    uuid_unparse(thread->uuid, uuid_str_thread);
-    uuid_unparse(thread->creator_uuid, uuid_str_client);
-    body = my_strcat(uuid_str_thread, "\r");
-    body = my_strcat_free(body, uuid_str_client, 1, 0);
-    body = my_strcat_free(body, "\r", 1, 0);
-    send_thread_display_info(body, client, thread);
+    while (thread) {
+        uuid_unparse(thread->uuid, uuid_str_thread);
+        if (my_strcmp(uuid_str_thread, uuid_str_client) == 0) {
+            uuid_unparse(thread->creator_uuid, uuid_str_client);
+            body = my_strcat(uuid_str_client, "\r");
+            body = my_strcat_free(body, uuid_str_client, 1, 0);
+            send_thread_display_info(body, client, thread);
+            return;
+        }
+        thread = thread->next;
+    }
 }
 
 void info_command(server_t *server, client_t *client, char **command)
