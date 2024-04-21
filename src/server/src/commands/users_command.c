0c16dc7ade73f4c2b83ae4ad9649abe3d4bea75d
Sebabacou <sebastien.bertilsouchet@protonmail.com>
2024-04-21 19:01:39 +0200

    fix: users display

--- src/server/src/commands/users_command.c
+++ src/server/src/commands/users_command.c
@@ -10,24 +10,21 @@
 #include "server.h"
 #include "my.h"
 
-static void print_user(client_t *client, client_t *to_sent,
-    client_list_t *connected)
+static void print_user(client_t *client, client_t *to_sent)
 {
     char uuid_str[37] = {0};
-    char status_str[12];
     char *body;
-    int connect = (get_client_by_uuid(connected, client->uuid)) ? 1 : 0;
-    request_t *request = create_request(PRINT_USERS, 200, NULL);
 
     uuid_unparse(client->uuid, uuid_str);
     body = my_strcat(uuid_str, "\r");
     body = my_strcat_free(body, client->username, 1, 0);
     body = my_strcat_free(body, "\r", 1, 0);
-    sprintf(status_str, "%d", connect);
-    body = my_strcat_free(body, status_str, 1, 0);
-    my_strncpy(request->body, body, MAX_BODY_LENGTH);
+    body = my_strcat_free(body, client->username ? "1" : "0", 1, 0);
+    body = my_strcat_free(body, "\r", 1, 0);
+    my_strncpy(body, body, MAX_BODY_LENGTH);
+    create_add_request_to_list(to_sent->requests_sent, NT_PRINT_USER, 200,
+                               body);
     free(body);
-    add_request_to_list(to_sent->requests_sent, request);
 }
 
 void users_command(UNUSED server_t *server, client_t *client,
@@ -41,7 +38,7 @@ void users_command(UNUSED server_t *server, client_t *client,
         return;
     }
     while (current) {
-        print_user(current, client, server->clients);
+        print_user(current, client);
         current = current->next;
     }
 }
